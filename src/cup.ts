598a3554c97c9c8357d16f3db91e3843c159bdbc
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-21 13:22:14 -0400

    fix: cup

--- src/cup.ts
+++ src/cup.ts
@@ -6,5 +6,5 @@ import { encoder } from "./encoder.ts";
  * @see {@link https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Ps;Ps-H.1D86}
  */
 export function cup(row: number, col: number): Uint8Array {
-  return encoder.encode(`\x1b[${row + 1};${col + 1}H`);
+  return encoder.encode(`\x1b[${row};${col}H`);
 }
