f239597c8e5fd80e874254a880b6a078b136589d
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-21 18:45:52 -0400

    feat: DEC private mode 1048

--- src/decrst.ts
+++ src/decrst.ts
@@ -16,6 +16,13 @@ export function decrst(mode: DECResetMode): Uint8Array {
  */
 export const enum DECResetMode {
   /**
+   * Restore cursor as in DECRC, xterm. This may be disabled by the titeInhibit resource.
+   *
+   * @see {@link https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-?-Pm-l:Ps-=-1-0-4-8.1F88}
+   */
+  P1048 = 1048,
+
+  /**
    * Use Normal Screen Buffer and restore cursor as in DECRC, xterm. This may be disabled by the titeInhibit resource. This combines the effects of the 1047  and 1048 modes. Use this with terminfo-based applications rather than the 47 mode.
    *
    * @see {@link https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-?-Pm-l:Ps-=-1-0-4-9.1F89}
