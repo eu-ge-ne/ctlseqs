18971c8adf24264dccf51918448db2c451e67bbc
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-21 18:32:01 -0400

    fix

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,6 @@
 export { cup } from "./cup.ts";
 export { decrc } from "./decrc.ts";
-export { decrst } from "./decrst.ts";
+export { DECResetMode, decrst } from "./decrst.ts";
 export { decsc } from "./decsc.ts";
-export { decset } from "./decset.ts";
+export { decset, DECSetMode } from "./decset.ts";
 export { ris } from "./ris.ts";
