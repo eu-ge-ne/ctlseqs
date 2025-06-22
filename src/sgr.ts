bb200c925630b0fba4feaec7f07db067e8c687d5
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-21 22:03:39 -0400

    fix

--- src/sgr.ts
+++ src/sgr.ts
@@ -1,5 +1,10 @@
 import { encoder } from "./encoder.ts";
 
+/**
+ * Character Attributes (SGR)
+ *
+ * @see {@link https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Pm-m.1CA7}
+ */
 export function sgr(...attrs: (SGRAttr | SGRColor)[]): Uint8Array {
   const y = attrs.map((x) => {
     if (typeof x === "number") {
