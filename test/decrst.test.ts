aaf206fbe9d877e661e40fabfb30e937eed4670e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-06-22 00:25:23 -0400

    test: decset, decrst (#13)

--- test/decrst.test.ts
+++ test/decrst.test.ts
@@ -3,6 +3,12 @@ import { assertEquals } from "@std/assert";
 import { DECResetMode, decrst } from "../src/mod.ts";
 
 Deno.test("decrst", () => {
+  const bytes = decrst();
+
+  assertEquals(bytes, new TextEncoder().encode(""));
+});
+
+Deno.test("decrst mode", () => {
   const bytes = decrst(DECResetMode.P1049);
 
   assertEquals(bytes, new TextEncoder().encode("\x1b[?1049l"));
--- test/decset.test.ts
+++ test/decset.test.ts
@@ -3,6 +3,12 @@ import { assertEquals } from "@std/assert";
 import { decset, DECSetMode } from "../src/mod.ts";
 
 Deno.test("decset", () => {
+  const bytes = decset();
+
+  assertEquals(bytes, new TextEncoder().encode(""));
+});
+
+Deno.test("decset mode", () => {
   const bytes = decset(DECSetMode.P1049);
 
   assertEquals(bytes, new TextEncoder().encode("\x1b[?1049h"));
