b2292e4ec17a862bd036d19243bb96c65993606b
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-29 17:22:06 -0400

    feat: DECTCEM, ALTSCR

--- src/decrst.ts
+++ src/decrst.ts
@@ -19,18 +19,14 @@ export function decrst(...modes: DECResetMode[]): Uint8Array {
  */
 export const enum DECResetMode {
   /**
-   * Restore cursor as in DECRC, xterm. This may be disabled by the titeInhibit resource.
-   *
-   * @see {@link https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-?-Pm-l:Ps-=-1-0-4-8.1F88}
+   * Hide cursor (DECTCEM), VT220.
    */
-  P1048 = 1048,
+  DECTCEM = 25,
 
   /**
    * Use Normal Screen Buffer and restore cursor as in DECRC, xterm. This may be disabled by the titeInhibit resource. This combines the effects of the 1047  and 1048 modes. Use this with terminfo-based applications rather than the 47 mode.
-   *
-   * @see {@link https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-?-Pm-l:Ps-=-1-0-4-9.1F89}
    */
-  P1049 = 1049,
+  ALTSCR = 1049,
 
   /**
    * End synchronized update
