6c13de2d36e3d9d1c136d8df14c91a5b329f7580
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-01 22:55:29 -0400

    test

--- test/cursor.test.ts
+++ test/cursor.test.ts
@@ -1,11 +1,29 @@
 import { assertEquals } from "@std/assert";
 
-import { cup, cuu } from "../src/mod.ts";
+import { cub, cud, cuf, cup, cuu } from "../src/mod.ts";
 
 Deno.test("cuu", () => {
-  const bytes = cuu(10);
+  const bytes = cuu(1);
 
-  assertEquals(bytes, new TextEncoder().encode("\x1b[10A"));
+  assertEquals(bytes, new TextEncoder().encode("\x1b[1A"));
+});
+
+Deno.test("cud", () => {
+  const bytes = cud(1);
+
+  assertEquals(bytes, new TextEncoder().encode("\x1b[1B"));
+});
+
+Deno.test("cuf", () => {
+  const bytes = cuf(1);
+
+  assertEquals(bytes, new TextEncoder().encode("\x1b[1C"));
+});
+
+Deno.test("cub", () => {
+  const bytes = cub(1);
+
+  assertEquals(bytes, new TextEncoder().encode("\x1b[1D"));
 });
 
 Deno.test("cup", () => {
